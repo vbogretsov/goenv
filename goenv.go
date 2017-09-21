91e5d097fc9a05f669b567f37b386e6e40c76a03
Vladimir Bogretsov <bogrecov@gmail.com>
2017-09-21 09:29:54 +0300

    fixed error message

--- goenv.go
+++ goenv.go
@@ -136,10 +136,10 @@ func main() {
 	}
 	err := initEnv(os.Args[1], os.Args[2])
 	if err != nil {
+		fmt.Fprintf(os.Stderr, "error: %s\n", err)
 		if _, err = os.Stat(os.Args[1]); err == nil {
 			os.RemoveAll(os.Args[1])
 		}
-		fmt.Fprintf(os.Stderr, "error: %s\n", err)
 		os.Exit(1)
 	}
 }
